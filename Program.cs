6a586f94698000069e33b57de791cdccbe745373
Dimo Todorov <baal98@gmail.com>
2022-03-25 00:18:21 +0200

    Destination Mapper

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Destination_Mapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            Regex regex = new Regex(@"(=|\/)(?'destination'[A-Z]{1}[A-Za-z]{2,})\1");
+
+            MatchCollection matches = regex.Matches(command);
+
+            int counter = 0;
+            List<string> list = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                list.Add(match.Groups["destination"].Value);
+
+                counter += match.Groups["destination"].Value.Count();
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", list)}");
+            Console.WriteLine($"Travel Points: {counter}");
+        }
+    }
+}
